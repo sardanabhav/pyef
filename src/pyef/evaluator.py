dd23a2856c7739f4f30c66603508b5e3a5ed0a40
Bhav Sardana <sardana.bhav@gmail.com>
2023-03-11 12:04:59 -0500

    update model gri search. testing not complete

--- src/pyef/evaluator.py
+++ src/pyef/evaluator.py
@@ -3,13 +3,13 @@ Evaluator
 """
 
 import math
-import os
+import pathlib
 from datetime import datetime
 from typing import Any, Dict, Union
 
 import numpy as np
 import pandas as pd
-from memo import Runner, grid, memfile
+from memo import Runner, grid, memfile, memlist
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import (
@@ -64,7 +64,13 @@ class Evaluator:
         mape = mean_absolute_percentage_error(y_true, y_pred)
         mae = mean_absolute_error(y_true, y_pred)
         rmse = math.sqrt(mean_squared_error(y_true, y_pred))
-        return {"mape": mape, "mae": mae, "rmse": rmse}
+        # peak_timing_error = self.peak_timing_error(y_true, y_pred)
+        return {
+            "mape": mape,
+            "mae": mae,
+            "rmse": rmse,
+            # "peak_timing_error": peak_timing_error
+        }
 
 
 class ModelGridSearch:
@@ -74,6 +80,7 @@ class ModelGridSearch:
         formulae: list[str],
         models: list[Regressor],
         pred_start_dates: list[datetime],
+        result_dir: str | None = None,
         save_plots: bool = False,
         horizon: int = 8760,
         n_jobs: int = 1,
@@ -85,17 +92,17 @@ class ModelGridSearch:
         self.save_plots = save_plots
         self.horizon = horizon
         self.n_jobs = n_jobs
-        self.file_path = os.path.join("data", "bigdeal2022")
+        self.result_dir = result_dir
 
     def _search(self) -> None:
         out_of_sample = []
         in_sample = []
         plots = []
-        for i, M in enumerate(self.formulae):
+        for M_index, M in enumerate(self.formulae):
             logger.debug(f"model: \n{M}")
             for model in self.models:
                 logger.debug(f"base forecaster: {model.__str__()}")
-                for j, pred_start in enumerate(self.pred_start_dates):
+                for CV_index, pred_start in enumerate(self.pred_start_dates):
                     logger.debug(f"pred start date: {pred_start}")
                     forecast = Forecaster(
                         data=self.data,
@@ -106,26 +113,26 @@ class ModelGridSearch:
                     )
                     forecast.get_forecast()
                     if self.save_plots:
-                        fig1, fig2 = forecast.plot()
+                        plot_dict = forecast.plot()
                         plots.append(
                             {
-                                "out_of_sample": fig1,
-                                "in_sample": fig2,
-                                "M": i,
+                                "out_of_sample": plot_dict["out_of_sample"],
+                                "in_sample": plot_dict["in_sample"],
+                                "M": M_index,
                                 "model": model.__str__(),
-                                "CV": j,
+                                "CV": CV_index,
                             }
                         )
 
                     metrics = Evaluator(forecaster=forecast)
                     in_sample_results = metrics.in_sample_metrics
-                    in_sample_results["M"] = i
+                    in_sample_results["M"] = M_index
                     in_sample_results["model"] = model.__str__()
-                    in_sample_results["CV"] = j
+                    in_sample_results["CV"] = CV_index
                     out_of_sample_results = metrics.out_of_sample_metrics
-                    out_of_sample_results["M"] = i
+                    out_of_sample_results["M"] = M_index
                     out_of_sample_results["model"] = model.__str__()
-                    out_of_sample_results["CV"] = j
+                    out_of_sample_results["CV"] = CV_index
                     in_sample.append(in_sample_results)
                     out_of_sample.append(out_of_sample_results)
 
@@ -136,96 +143,112 @@ class ModelGridSearch:
     def _new_search(
         self,
     ) -> None:
-        for model in self.models:
-            model_name = model.__str__().replace("(", "").replace(")", "")
-
-            @memfile(
-                filepath=f"{self.file_path}/result_{model_name}.jsonl",
+        if self.result_dir is None:
+            results = []
+            memodecorator = memlist(data=results)
+        else:
+            pathlib.Path(
+                pathlib.Path.home().joinpath(".config", "pyef", "logs", self.result_dir)
+            ).mkdir(parents=True, exist_ok=True)
+            self.file_path = pathlib.Path.home().joinpath(
+                ".config", "pyef", "logs", self.result_dir
+            )
+            results_file = f"{self.file_path}/results.jsonl"
+            memodecorator = memfile(
+                filepath=results_file,
                 skip=True,
             )
-            def _execute_model(
-                M: str, pred_start: datetime, horizon: int
-            ) -> dict[str, float]:
-                forecast = Forecaster(
-                    data=self.data,
-                    formula=M,
-                    model=model,
-                    pred_start=pred_start,
-                    horizon=horizon,
-                )
-                forecast.get_forecast()
-                if self.save_plots:
-                    prediction = forecast.pred.copy()
-                    prediction["formula"] = M
-                    prediction["model"] = f"{model.__str__()}"
-                    prediction["pred_start"] = pred_start
-                    prediction.to_csv(
-                        f"{self.file_path}/predictions{model_name}.csv",
-                        mode="a",
-                    )
-                metrics = Evaluator(forecaster=forecast)
-                return {
-                    "mape": metrics.out_of_sample_metrics["mape"],
-                    "mape_in_sample": metrics.in_sample_metrics["mape"],
-                }
 
-            settings = grid(
-                M=self.formulae,
-                pred_start=self.pred_start_dates,
-                horizon=[self.horizon],
+        @memodecorator
+        def _execute_model(
+            M: str, model: Regressor, pred_start: datetime, horizon: int
+        ) -> dict[str, float]:
+            model_name = model.__str__().replace("(", "").replace(")", "")
+            forecast = Forecaster(
+                data=self.data,
+                formula=M,
+                model=model,
+                pred_start=pred_start,
+                horizon=horizon,
             )
-            runner = Runner(backend="loky", n_jobs=self.n_jobs)
-            runner.run(func=_execute_model, settings=settings, progbar=True)
+            forecast.get_forecast()
+            if self.save_plots:
+                fig_path = pathlib.Path.home().joinpath(
+                    ".config",
+                    "pyef",
+                    "figures",
+                    str(horizon),
+                    str(pred_start),
+                    model_name,
+                    M,
+                )
+                pathlib.Path(fig_path).mkdir(parents=True, exist_ok=True)
+                plot_dict = forecast.plot()
+                plot_dict["in_sample"].write_html(
+                    pathlib.Path.home().joinpath(fig_path, "in_sample.html")
+                )
+                plot_dict["out_of_sample"].write_html(
+                    pathlib.Path.home().joinpath(fig_path, "out_of_sample.html")
+                )
+            metrics = Evaluator(forecaster=forecast)
+            return {
+                "out_of_sample": metrics.out_of_sample_metrics,
+                "in_sample": metrics.in_sample_metrics,
+            }
+
+        settings = grid(
+            M=self.formulae,
+            model=self.models,
+            pred_start=self.pred_start_dates,
+            horizon=[self.horizon],
+        )
+        runner = Runner(backend="loky", n_jobs=self.n_jobs)
+        runner.run(func=_execute_model, settings=settings, progbar=True)
+
+        if self.result_dir is None:
+            results_df = pd.DataFrame(results)
+            results_df.loc[:, "model"] = (
+                results_df["model"]
+                .astype(str)
+                .str.replace(r"(", "", regex=True)
+                .str.replace(r")", "", regex=True)
+            )
+        else:
+            results_df = pd.read_json(results_file, lines=True)
+        self.grid_in_sample = results_df.loc[
+            :, ["M", "pred_start", "horizon", "model"]
+        ].join(pd.json_normalize(results_df.pop("in_sample")))
+        self.grid_out_of_sample = results_df.loc[
+            :, ["M", "pred_start", "horizon", "model"]
+        ].join(pd.json_normalize(results_df.pop("out_of_sample")))
+        # self.figures = pd.DataFrame(plots)
 
     def run_search(self) -> None:
         self._new_search()
 
     def get_best_model(self) -> None:
         self.run_search()
-        avg_out_of_sample = self.grid_out_of_sample.groupby(["M", "model"]).mean()
+        avg_out_of_sample = (
+            self.grid_out_of_sample.loc[:, ["M", "model", "mape", "mae", "rmse"]]
+            .groupby(["M", "model"])
+            .mean(numeric_only=True)
+        )
         selection_mae = avg_out_of_sample["mae"].idxmin()
         selection_mape = avg_out_of_sample["mape"].idxmin()
         selection_rmse = avg_out_of_sample["rmse"].idxmin()
 
         self.selected_model_mae = {
-            "formula_index": f"{selection_mae[0]}",
-            "formula": self.formulae[selection_mae[0]],
+            "formula": selection_mae[0],
             "model": selection_mae[1],
             "mae": avg_out_of_sample["mae"].min(),
         }
         self.selected_model_mape = {
-            "formula_index": f"{selection_mape[0]}",
-            "formula": self.formulae[selection_mape[0]],
+            "formula": selection_mape[0],
             "model": selection_mape[1],
             "mape": avg_out_of_sample["mape"].min(),
         }
         self.selected_model_rmse = {
-            "formula_index": f"{selection_rmse[0]}",
-            "formula": self.formulae[selection_rmse[0]],
+            "formula": selection_rmse[0],
             "model": selection_rmse[1],
             "rmse": avg_out_of_sample["rmse"].min(),
         }
-
-
-# import numpy as np
-# from memo import memlist, memfile, grid, time_taken, Runner
-
-# data = []
-
-# @memfile(filepath="results.jsonl")
-# @memlist(data=data)
-# @time_taken()
-# def birthday_experiment(class_size, n_sim):
-#     """Simulates the birthday paradox. Vectorized = Fast!"""
-#     sims = np.random.randint(1, 365 + 1, (n_sim, class_size))
-#     sort_sims = np.sort(sims, axis=1)
-#     n_uniq = (sort_sims[:, 1:] != sort_sims[:, :-1]).sum(axis = 1) + 1
-#     proba = np.mean(n_uniq != class_size)
-#     return {"est_proba": proba}
-
-# # declare all the settings to loop over
-# settings = grid(class_size=range(20, 30), n_sim=[100, 10_000, 1_000_000])
-
-# # use a runner to run over all the settings
-# runner = Runner(backend="threading", n_jobs=-1)
-# runner.run(func=birthday_experiment, settings=settings, progbar=True)
